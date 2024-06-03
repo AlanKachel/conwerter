3c0fa3c9b5b8faf7eb4b8d89288c0692b5dc267c
Alan <alankachel05@gmail.com>
2024-06-03 23:00:44 +0200

    d

--- installResources.ps1
+++ installResources.ps1
@@ -1,8 +1,3 @@
 pip install pyyaml
 pip install xmltodict
 pip install pyinstaller
-
-
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name PowerShellGet -Force -AllowClobber
